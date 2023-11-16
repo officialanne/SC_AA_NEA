0b55a54337eab22536ff23c407a5dad1954276e7
officialanne <anneasare7@gmail.com>
2023-11-16 19:04:27 +0000

    file starting

--- sign.js
+++ sign.js
@@ -1,9 +1,16 @@
+var fs = require('fs-js');
+fs.appendFile('myfile.txt', 'hello', (err) => {
+    if (err) throw err;
+    console.log('The "data to append" was appended to file!');
+    });
+
+
 
 function foo(){
     var name = document.getElementById("name").value;
     console.log(name);
     String(name);
-
+    
 }
 
 function setup(){
