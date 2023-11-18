d818d90862dc2ec5f880bd91e553d92c0437684a
officialanne <anneasare7@gmail.com>
2023-11-18 14:40:16 +0000

    php

--- sign.js
+++ sign.js
@@ -1,17 +1,10 @@
-var fs = require('fs-js');
-fs.appendFile('myfile.txt', 'hello', (err) => {
-    if (err) throw err;
-    console.log('The "data to append" was appended to file!');
-    });
-
 
 
 function foo(){
     var name = document.getElementById("name").value;
     console.log(name);
-    String(name);
-    
 }
+
 
 function setup(){
     createCanvas(600,400);
@@ -20,3 +13,4 @@ function setup(){
 function draw(){
     background(190,200,210);
 }
+
