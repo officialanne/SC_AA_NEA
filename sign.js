bab3613a5baddcbc43e323d521208689d08488a1
officialanne <anneasare7@gmail.com>
2023-11-19 16:58:35 +0000

    trying js files

--- sign.js
+++ sign.js
@@ -1,11 +1,18 @@
-
+var fs = require("fs");
 
 function foo(){
     var name = document.getElementById("name").value;
     console.log(name);
-
+    fs = require("fs");
+    fs.writeFile("details.txt", String(name), (err) =>{
+        // In case of a error throw err. 
+        if (err) throw err;
+    })
 }
 
+function writing(){
+    
+}
 
 function setup(){
     createCanvas(600,400);
