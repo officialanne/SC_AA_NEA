d9f8a09f2ec0ce81a2725d1067fd30e49da780ae
officialanne <anneasare7@gmail.com>
2023-11-21 20:57:00 +0000

    saving with local storage

--- sign.js
+++ sign.js
@@ -1,14 +1,15 @@
-var fs = require("fs");
+
 
 function foo(){
     var name = document.getElementById("name").value;
     console.log(name);
-    fs = require("fs");
-    fs.writeFile("details.txt", String(name), (err) =>{
-        // In case of a error throw err. 
-        if (err) throw err;
-    })
 }
+
+function store(){
+    var inputEmail = document.getElementById("email");
+    localStorage.setItem("email"), inputEmail.value;
+    console.log(inputEmail)
+  }
 
 function writing(){
     
