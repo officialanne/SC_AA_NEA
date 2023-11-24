3d748c0454cd2b1fa6734a0c25a44c387e5ad805
officialanne <144495906+officialanne@users.noreply.github.com>
2023-11-24 11:56:38 +0000

    starting student subsystem with js

---
+++ notes.js
@@ -0,0 +1,20 @@
+let into;
+
+
+function setup() {
+    textAlign(CENTER);
+    createCanvas(1000, 600);
+    
+    
+    
+}
+
+function draw(){
+    background("#ccd5ae");
+    rect(250, 20, 500, 55);
+    rect(50, 125, 150, 75);
+    rect(250, 125, 150, 75);
+
+
+
+}
