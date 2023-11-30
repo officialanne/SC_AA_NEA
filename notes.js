7183c91231d7744df94303552172667179f0087f
officialanne <anneasare7@gmail.com>
2023-11-30 16:39:40 +0000

    summary notes menu outline

--- notes.js
+++ notes.js
@@ -1,17 +1,35 @@
+let boxes;
+let intro_text = "Study these notes to help catch up on content";
+let box_text = ["Unit 1", "Unit 2", "Unit 3", "Unit 4", "Unit 5", "Unit 6", "Unit 7", "Unit 8", "Unit 9", "Unit 10", "Unit 11", "Unit 12"];
+let intro, box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11, box12; 
+
 
 function setup() {
     textAlign(CENTER);
     createCanvas(1000, 600);
+    intro = new Sprite(width/2, 50, 500, 55);
 
+    boxes = new Group();
+
+    box1 = new boxes.Sprite(150, 175, 150, 75);
+    box2 = new boxes.Sprite(375, 175, 150, 75);
+    box3 = new boxes.Sprite(625, 175, 150, 75);
+    box4 = new boxes.Sprite(850, 175, 150, 75);
+    box5 = new boxes.Sprite(150, 325, 150, 75);
+    box6 = new boxes.Sprite(375, 325, 150, 75);
+    box7 = new boxes.Sprite(625, 325, 150, 75);
+    box8 = new boxes.Sprite(850, 325, 150, 75);
+    box9 = new boxes.Sprite(150, 500, 150, 75);
+    box10 = new boxes.Sprite(375, 500, 150, 75);
+    box11 = new boxes.Sprite(625, 500, 150, 75);
+    box12 = new boxes.Sprite(850, 500, 150, 75);
     
     
 }
 
 function draw(){
     background("#ccd5ae");
-    rect(250, 20, 500, 55);
-    rect(50, 125, 150, 75);
-    rect(250, 125, 150, 75);
+    intro.text=(intro_text);
 
 
 
