772b937636bf9c73c75138e502f4e926b1dd2b7c
officialanne <anneasare7@gmail.com>
2023-12-01 15:03:15 +0000

    fixing html

--- trial.py
+++ trial.py
@@ -7,16 +7,18 @@ from flask import Flask, app, render_template, request
 con = sqlite3.connect("users.db")
 
 cur = con.cursor()
-app = Flask(__name__)
-@app.route('/trial.py', methods = ["GET", "POST"])
-def gfg():
-    if request.method == "POST":
-       # getting input with name = fname in HTML form
-       first_name = request.form.get("fname")
-       # getting input with name = lname in HTML form 
-       last_name = request.form.get("lname") 
-       return "Your name is "+first_name + last_name
-    return render_template("signup.html")
 
-if __name__=='__main__':
-   app.run()
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+   return render_template('signup.html')
+
+app.route('/forms', methods=['POST'])
+def getValue():
+   name = request.form['fname']
+   print(name)
+   return render_template("index.html", n=name)
+
+
+index()
