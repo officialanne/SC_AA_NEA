a452e273358959018332d711f26e258ce8ff1eb2
officialanne <144495906+officialanne@users.noreply.github.com>
2023-12-12 15:26:02 +0000

    files from NEA_Synchronise_AA

--- notes.js
+++ notes.js
@@ -1,5 +1,9 @@
 let boxes;
+let userChoice;
+let returnMenu;
 let intro_text = "Study these notes to help catch up on content";
+let unitNotes = ["FDE Cycle = Fetch, Decode, Execute", "Memory Management", "The Waterfall Model", "Asymmetric Encryption", "Structure of the Internet", "Binary Arithmetic", "Abstract Data Types", "Boolean Algebra", "The Data Protection Act", "Thinkint Abstractly", "Iteration", "A* Algorithm"];
+let unitFrame = ["Here are the notes for unit ", "Key terms and definitions: ", "Key advantages: ", "Key disadvantages: "];
 let box_text = ["Unit 1", "Unit 2", "Unit 3", "Unit 4", "Unit 5", "Unit 6", "Unit 7", "Unit 8", "Unit 9", "Unit 10", "Unit 11", "Unit 12"];
 let intro, box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11, box12; 
 
@@ -43,5 +47,94 @@ function draw(){
     box10.text = box_text[9];
     box11.text = box_text[10];
     box12.text = box_text[11];
+
+    function selectUnit() {
+        //change background colour
+        background("#a3cef1");
+
+        //reposition buttons
+        box1.pos = { x: 3000, y: 3000 };
+        box2.pos = { x: 3500, y: 3500 };
+        box3.pos = { x: 4000, y: 4000 };
+        box4.pos = { x: 4500, y: 4500 };
+        box5.pos = { x: 5000, y: 5000 };
+        box6.pos = { x: 5500, y: 5500 };
+        box7.pos = { x: 6000, y: 6000 };
+        box8.pos = { x: 6500, y: 6500 };
+        box9.pos = { x: 2000, y: 2000 };
+        box10.pos = { x: 2500, y: 2500 };
+        box11.pos = { x: -3000, y: -3000 };
+        box12.pos = { x: -3500, y: -3500 };
+
+        if (box1.mouse.presses()) {
+            choice = 1;
+            box1.text = "Here are the notes for unit 1";
+        }
+        else if (box2.mouse.presses()) {
+            choice = 2;
+            box2.text = "Here are the notes for unit 2";
+        }
+
+        else if (box3.mouse.presses()) {
+            choice = 3;
+            box3.text = "Here are the notes for unit 3";
+        }
+
+        else if (box4.mouse.presses()) {
+            choice = 4;
+            box4.text = "Here are the notes for unit 4";
+        }
+
+        else if (box5.mouse.presses()) {
+            choice = 5;
+            box5.text = "Here are the notes for unit 5";
+        }
+
+        else if (box6.mouse.presses()) {
+            choice = 6;
+            box6.text = "Here are the notes for unit 6";
+        }
+
+        else if (box7.mouse.presses()) {
+            choice = 7;
+            box7.text = "Here are the notes for unit 7";
+
+        }
+
+        else if (box8.mouse.presses()) {
+            choice = 8;
+            box8.text = "Here are the notes for unit 8";
+        }
+
+        else if (box9.mouse.presses()) {
+            choice = 9;
+            box9.text = "Here are the notes for unit 9";
+        }
+
+        else if (box10.mouse.presses()) {
+            choice = 10;
+            box10.text = "Here are the notes for unit 10";
+        }
+
+        else if (box11.mouse.presses()) {
+            choice = 11;
+            box11.text = "Here are the notes for unit 11";
+        }
+
+        else if (box12.mouse.presses()) { 
+            choice = 12;
+            box12.text = "Here are the notes for unit 12";
+        }
+
+
+
+    }
+
+    if (box1.mouse.presses()) {
+        box1.text = random(unitNotes);
+    }
+
+    
+
 
 }
