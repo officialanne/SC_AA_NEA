8ade0a83c735c7794b4d6cb8db6b5515d6a3c6c0
officialanne <anneasare7@gmail.com>
2023-12-12 17:07:33 +0000

    flashcard infrastructure setup

---
+++ flash.js
@@ -0,0 +1,16 @@
+let unitOneQ = ["qOne", "qTwo", "qThree"];
+let unitOneA = ["aOne", "aTwo", "aThree"];
+
+function setup(){
+    textAlign(CENTER);
+    createCanvas(1000, 600);
+    background("purple");
+}
+
+function draw(){
+
+}
+
+function selectCards(){
+    
+}
