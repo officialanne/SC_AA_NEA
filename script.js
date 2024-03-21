0281b6fa5211ebdfb3c266d23fabf320d1bcb8a4
officialanne <anneasare7@gmail.com>
2024-03-21 23:16:41 +0000

    comments

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 
+// this variable stores the number of times the button has been pressed
+let numButtonClicks = 0;
 
-//let numButtonClicks = 0;
-
-/*
+// this function is called whenever the button is pressed in HTML
 function buttonClicked() {
+    // increment by 1 and display the new number
     numButtonClicks = numButtonClicks + 1;
     document.getElementById("mainDiv").textContent =
         "Button Clicked times: " + numButtonClicks;
      
 }
-*/
+
 
 // this function runs once at the beginning of the program
 // it puts the canvas on the web page
